18b91bb68ba389508882ab711da29bbf16eaeb35
DeogratiousT <deogratioust@gmail.com>
2022-03-23 15:25:26 +0300

    logout

---
+++ app/GraphQL/Mutations/User/Logout.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\GraphQL\Mutations\User;
+
+use App\Models\User;
+use GraphQL\Type\Definition\ResolveInfo;
+use App\GraphQL\Exceptions\AuthenticationException;
+use Nuwave\Lighthouse\Support\Contracts\GraphQLContext;
+
+class Logout
+{
+    /**
+     * Return a value for the field.
+     *
+     * @param  @param  null  $root Always null, since this field has no parent.
+     * @param  array<string, mixed>  $args The field arguments passed by the client.
+     * @param  \Nuwave\Lighthouse\Support\Contracts\GraphQLContext  $context Shared between all fields.
+     * @param  \GraphQL\Type\Definition\ResolveInfo  $resolveInfo Metadata for advanced query resolution.
+     * @return mixed
+     */
+    public function __invoke($root, array $args, GraphQLContext $context, ResolveInfo $resolveInfo)
+    {
+        $user = User::where('email', $args['email'])->first();
+    
+        if ($user->currentAccessToken() == null) {
+            throw new AuthenticationException('Authentication exception', 'User has no active access tokens');
+        }
+
+        $user->tokens()->delete();
+
+        return [
+            'user' => $user,
+            'message' => 'User Logged Out'
+        ];
+    }
+}
